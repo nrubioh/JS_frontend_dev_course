6b0ea30aea3caa27f967e4b3c962f2ba3408bc04
nrubioh <nrubiohoskins@gmail.com>
2023-10-10 13:31:18 -0300

    aside, product detail added , and fns toggle, add displays

--- main.js
+++ main.js
@@ -1,18 +1,31 @@
 const nav_email = document.querySelector('.navbar-email');
 const desktop_menu = document.querySelector('.desktop-menu');
 
-const threeLines = document.querySelector('.menu');
+const threeLinesIcon = document.querySelector('.menu');
 const mobile_menu = document.querySelector('.mobile-menu');
 
+const shopIcon = document.querySelector('.navbar-shopping-cart');
+const product_menu = document.querySelector('.product-detail');
+
 nav_email.addEventListener('click',toggleDesktopMenu);
-threeLines.addEventListener('click',toggleMobileMenu);
+threeLinesIcon.addEventListener('click',toggleMobileMenu);
+shopIcon.addEventListener('click',toggleProductMenu);
 
 function toggleDesktopMenu() {
-    console.log('click');
     desktop_menu.classList.toggle('inactive');
+        console.log('click');
+        product_menu.classList.add('inactive');
 }
 
 function toggleMobileMenu() {
-    console.log('mobile click');
     mobile_menu.classList.toggle('inactive');
+        console.log('mobile click');
+        product_menu.classList.add('inactive');
+}
+
+function toggleProductMenu() {
+    product_menu.classList.toggle('inactive');
+        console.log('Product menu click');
+        desktop_menu.classList.add('inactive');
+        mobile_menu.classList.add('inactive');
 }
