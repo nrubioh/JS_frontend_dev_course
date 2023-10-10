14efa0f2cf851abe34eb4b41ed6a429ca88ab88d
nrubioh <nrubiohoskins@gmail.com>
2023-10-10 09:12:52 -0300

    mobile-menu fusion, mobile toggle click, media query displays

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 const nav_email = document.querySelector('.navbar-email');
 const desktop_menu = document.querySelector('.desktop-menu');
 
+const threeLines = document.querySelector('.menu');
+const mobile_menu = document.querySelector('.mobile-menu');
+
 nav_email.addEventListener('click',toggleDesktopMenu);
+threeLines.addEventListener('click',toggleMobileMenu);
 
 function toggleDesktopMenu() {
     console.log('click');
     desktop_menu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    console.log('mobile click');
+    mobile_menu.classList.toggle('inactive');
 }
